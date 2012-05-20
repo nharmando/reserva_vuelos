ca3b7354e0adfb2b7c76bbec0ccfb8d2e8c43237
Armando NH <armando.nh@gmail.com>
2012-05-19 21:56:35 -0600

    Segundo commit colocando contenido a index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Reserva de vuelos</title>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="css/estilos.css" />
+</head>
+ 
+<body>
+    <header>
+       <h1>Reservaci&oacute;n de vuelos</h1>
+    </header>
+    <section>
+       <article>
+           <h2>Titilo de contenido<h2>
+           <p> Contenido (ademas de imagenes, citas, videos etc.) </p>
+       </article>
+    </section>
+    <aside>
+       <h3>Titulo de contenido</h3>
+           <p>contenido</p>
+    </aside>
+    <footer>
+        Creado por Armando NH
+    </footer>
+</body>
+</html>
